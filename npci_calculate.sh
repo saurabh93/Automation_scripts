2489c35055f0bfae9e675ef16e46d909123a82f3
saurabh93 <saurabhtewari93@gmail.com>
2015-01-26 23:48:33 +0530

    npci_calculate.sh added to repository

---
+++ npci_calculate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "Getting DC2 NPCI"
+#cat /home/ussd/SIP_Manager/MIS/NPCI_MIS/NPCI_DC2_`TZ=GMT+24 date +%Y%m%d`.txt | sed -n '0~4p' > npcidc2.txt 
+#cat /home/ussd/SIP_Manager/MIS/NPCI_MIS/NPCI_DC2_`date +%Y%m%d`.txt | sed -n '0~4p' >> npcidc2.txt
+
+#echo -e "Running command in DR1.Enter DR1 password"
+#cat /home/ussd/SIP_Manager/MIS/NPCI_MIS/NPCI_DR1__`TZ=GMT+24 date +%Y%m%d`.txt > /tmp/npcidr1.txt
+#cat /home/ussd/SIP_Manager/MIS/NPCI_MIS/NPCI_DR1__`date +%Y%m%d`.txt >> /tmp/npcidr1.txt
+
+#echo -e
+#cat /home/ussd/SIP_Manager/MIS/NPCI_MIS/NPCI_DR2_`TZ=GMT+24 date +%Y%m%d`.txt | sed -n '0~4p'> npcidr2.txt
+#cat /home/ussd/SIP_Manager/MIS/NPCI_MIS/NPCI_DR2_`date +%Y%m%d`.txt | sed -n '0~4p' >> npcidr2.txt
+
+#cat /home/ussd/SIP_Manager/MIS/NPCI_MIS/NPCI_DC1_`TZ=GMT+24 date +%Y%m%d`.txt | sed -n '0~4p'> npcidc1.txt 
+#cat /home/ussd/SIP_Manager/MIS/NPCI_MIS/NPCI_DC1_`date +%Y%m%d`.txt | sed -n '0~4p' >> npcidc1.txt
+
+
+#cat /home/SOAP/NPCI/log/`TZ=GMT+24 date +%Y%-m%d`-req.txt | grep -v BARRED* | wc -l
+ 
+
